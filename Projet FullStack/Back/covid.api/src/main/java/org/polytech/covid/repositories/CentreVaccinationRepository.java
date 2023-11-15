2fb559d2d73de096c1e552b355cc776db30c8303
ArnaudBrg <147829966+ArnaudBrg@users.noreply.github.com>
2023-11-15 18:52:56 +0100

    Delete Projet FullStack directory

--- Projet FullStack/Back/covid.api/src/main/java/org/polytech/covid/repositories/CentreVaccinationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.polytech.covid.repositories;
-        import org.polytech.covid.domain.CentreVaccination;
-        import org.springframework.data.repository.CrudRepository;
-
-public interface CentreVaccinationRepository extends CrudRepository<CentreVaccination, Long> {
-    CentreVaccination findByNom(String nom);
-}
